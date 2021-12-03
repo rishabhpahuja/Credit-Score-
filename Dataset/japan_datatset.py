6bf8ee9e17d2a5e56921ac51cdcf8f74bc3e7d6d
rishabhpahuja <rishabhpahuja27@gmail.com>
2021-12-03 17:56:28 -0500

    japan dataset clean code

---
+++ Dataset/japan_datatset.py
@@ -0,0 +1,64 @@
+import numpy as np
+from numpy.core.defchararray import index
+from numpy.lib.function_base import append
+import pandas as pd
+from csv import writer
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
+
+japan_dataset=pd.read_csv('./UCI Data/Japan/crx.data',header=None,sep=',')
+japan_label=japan_dataset.loc[:,len(japan_dataset.columns)-1]
+japan_dataset=japan_dataset.drop(japan_dataset.columns[[len(japan_dataset.columns)-1]],axis=1)
+japan_label=japan_label.replace(['+','-'],[1,0])
+japan_label.to_csv('Japan_label.csv',index=False,header=False)
+
+# print(japan_dataset)
+# print(len(np.where(japan_dataset.loc[:,14]=='')[0]))
+
+index=list()
+for i in range(len(japan_dataset.columns)):
+    empty_indx=np.where(japan_dataset.loc[:,i]=='?')
+    index=index+empty_indx[0].tolist()
+
+remove=list()
+for i in range(len(index)):
+    if index[i] not in remove:
+        remove.append(index[i])
+
+japan_dataset=japan_dataset.drop(labels=remove,axis=0)
+
+
+
+
+
+
+# Performing one hot encoding for categorical classification
+japan_dataset=pd.get_dummies(japan_dataset,columns=[0,3,4,5,6,8,9,11,12])
+japan_dataset.loc[:,1]=pd.to_numeric(japan_dataset.loc[:,1], downcast="float")
+japan_dataset.loc[:,13]=japan_dataset.loc[:,13].astype(int)
+japan_dataset.to_csv('Japan_Data.csv',index=False,header=False)
+
+# for i in (japan_dataset.columns):
+#     if japan_dataset[i].dtype == str:
+#         print('yes')
+#     else:
+#         print('no')
+print(japan_dataset.dtypes)
+print(japan_dataset)
+
+'''
+Applying PCA
+'''
+cov=(japan_dataset.T@japan_dataset)/(len(japan_dataset)-1)
+sorted_eig_vals,sorted_eig_vecs=np.linalg.eig(cov)
+
+variance=np.zeros(len(japan_dataset.columns))
+for i in range(len(japan_dataset.columns)):
+    variance[i]=sum(sorted_eig_vals[:i+1])/sum(sorted_eig_vals)
+
+print(variance)
+
+new_japan_dataset=japan_dataset@sorted_eig_vecs[:,:2]
