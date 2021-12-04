ada693535633504f4ca6785bdfdd0555c4a7ec5d
rishabhpahuja <rishabhpahuja27@gmail.com>
2021-12-04 01:31:15 -0500

    Japan Dataset

--- Dataset/japan_datatset.py
+++ Dataset/japan_datatset.py
@@ -10,10 +10,9 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 
 japan_dataset=pd.read_csv('./UCI Data/Japan/crx.data',header=None,sep=',')
-japan_label=japan_dataset.loc[:,len(japan_dataset.columns)-1]
-japan_dataset=japan_dataset.drop(japan_dataset.columns[[len(japan_dataset.columns)-1]],axis=1)
-japan_label=japan_label.replace(['+','-'],[1,0])
-japan_label.to_csv('Japan_label.csv',index=False,header=False)
+
+
+
 
 # print(japan_dataset)
 # print(len(np.where(japan_dataset.loc[:,14]=='')[0]))
@@ -29,7 +28,10 @@ for i in range(len(index)):
         remove.append(index[i])
 
 japan_dataset=japan_dataset.drop(labels=remove,axis=0)
-
+japan_label=japan_dataset.loc[:,len(japan_dataset.columns)-1]
+japan_dataset=japan_dataset.drop(japan_dataset.columns[[len(japan_dataset.columns)-1]],axis=1)
+japan_label=japan_label.replace(['+','-'],[1,0])
+japan_label.to_csv('Japan_label.csv',index=False,header=False)
 
 
 
@@ -59,6 +61,65 @@ variance=np.zeros(len(japan_dataset.columns))
 for i in range(len(japan_dataset.columns)):
     variance[i]=sum(sorted_eig_vals[:i+1])/sum(sorted_eig_vals)
 
-print(variance)
+# print(sorted_eig_vecs)
 
 new_japan_dataset=japan_dataset@sorted_eig_vecs[:,:2]
+print(new_japan_dataset)
+
+'''
+Performing KNN
+'''
+
+scores=list()
+X_train,X_test,y_train,y_test=train_test_split(new_japan_dataset,japan_label,test_size=0.2,random_state=42)
+for k in range(1,35):
+    knn=KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train,y_train)
+    y_pred=knn.predict(X_test)
+    scores.append(metrics.accuracy_score(y_test,y_pred))
+
+plt.subplot(1, 2, 1)
+plt.title('With Outlier')
+plt.plot(range(1,35),scores)
+
+
+i=5
+while True:
+    DBSCAN_model=DBSCAN(eps=i, min_samples=10).fit(new_japan_dataset)
+    labels=DBSCAN_model.labels_
+    i+=5
+    print(len(np.where(labels==-1)[0]))
+    if len(np.where(labels==-1)[0])<=0.055*len(new_japan_dataset) and len(np.where(labels==-1)[0])>=0.04*len(japan_label):
+        # print(len(np.where(labels==-1)[0]))
+        print(i)
+        break
+japan_dataset_no_outlier=new_japan_dataset.loc[labels!=-1,:]
+japan_label_no_outlier=japan_label.loc[labels!=-1]
+
+'''
+Performing KNN
+'''
+
+scores_no_outlier=list()
+X_train_no_outlier,X_test_no_outlier,y_train_no_outlier,y_test_no_outlier=train_test_split(japan_dataset_no_outlier,japan_label_no_outlier,test_size=0.2,random_state=42)
+for k in range(1,35):
+    knn=KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train_no_outlier,y_train_no_outlier)
+    y_pred=knn.predict(X_test_no_outlier)
+    scores_no_outlier.append(metrics.accuracy_score(y_test_no_outlier,y_pred))
+
+plt.subplot(1, 2, 2)
+plt.title('Without Outlier')
+plt.plot(range(1,35),scores_no_outlier)
+plt.show()
+
+
+plt.subplot(1, 2, 1)
+plt.plot(new_japan_dataset.loc[:,0],new_japan_dataset.loc[:,1],'x')
+plt.title('Points after PCA')
+
+plt.subplot(1, 2, 2)
+plt.plot(new_japan_dataset.loc[labels==-1,0],new_japan_dataset.loc[labels==-1,1],'x')
+plt.title('Points removed')
+plt.show()
+
