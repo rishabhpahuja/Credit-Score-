31c67dba9e16d9af35ed221f4d061894acbff2cf
Aditya Rathi <adityamr@andrew.cmu.edu>
2021-12-06 21:25:41 -0500

    Plotter update and accuracy outputs

--- plotter.py
+++ plotter.py
@@ -8,9 +8,10 @@ data = pd.read_csv('accuracy.csv',header=None)
 datasets = set(data.iloc[:,2])
 for i in datasets:
     new_data = data.loc[data.loc[:,2]==i,:]
-    g = sns.catplot(data=new_data,kind='bar',x=0,y=1,hue=3)
+    g = sns.catplot(data=new_data,kind='bar',x=0,y=1,hue=3,legend=False)
     g.set(xlabel="Method",ylabel="Accuracy ",title=str('Dataset:'+i))
     g.set_xticklabels(rotation=30)
+    plt.legend(loc='upper right')
     plt.savefig(str('accuracy_plots/'+i+'.png'),bbox_inches='tight')
-    plt.show()
+    #plt.show()
 
